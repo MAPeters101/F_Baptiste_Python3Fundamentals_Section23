380701e1983f90583526d6f952e240696d117a33
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:37:39 -0400

    Description for all exercise04.

---
+++ exercise04.py
@@ -0,0 +1,23 @@
+'''
+Question 4
+Using both methods of generating throws, build a dictionary that contains the
+face values as keys, and the number of times they were selected in the
+simulated throws.
+
+For example, assuming you made 100 throws using one of these methods, your
+dictionary might look like this:
+
+{
+    '9': 39,
+    '10': 27,
+    'J': 28,
+    'Q': 34,
+    'K': 36,
+    'A': 36
+}
+Note that your values in the dictionary should add up to 200 is you made one
+100 throws.
+
+Write a function that is given the function to use to generate the throws, the
+number of throws to simulate, and returns this dictionary.
+'''

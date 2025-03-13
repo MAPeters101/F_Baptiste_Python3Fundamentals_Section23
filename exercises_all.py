785914270178bd97f1638f58b25163b92bb7ef63
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:34:05 -0400

    Reformat descriptions for all exercises..

--- exercises_all.py
+++ exercises_all.py
@@ -1,10 +1,12 @@
 '''
 Question 1
-Generate the sample space of rolling two 6-sided dice, numbered '9', '10', 'J', 'Q', 'K', 'A'.
+Generate the sample space of rolling two 6-sided dice, numbered '9', '10', 'J',
+'Q', 'K', 'A'.
 
 (The sample space is the set of all possible outcomes).
 
-Your result should be a list containing tuples for the outcome of each die, e.g.
+Your result should be a list containing tuples for the outcome of each die,
+e.g.
 
 [('9', '9'),
  ('9', '10'),
@@ -22,21 +24,28 @@ Your result should be a list containing tuples for the outcome of each die, e.g.
 Make this a function that returns the sample space, called make_sample_space.
 
 Question 2
-Using the sample space you just created above, simulate throwing the two die n times by making random choices from the sample space.
+Using the sample space you just created above, simulate throwing the two die n
+times by making random choices from the sample space.
 
-Again, make this into a function that returns the random choices as a list of tuples, with n as a parameter of this function.
+Again, make this into a function that returns the random choices as a list of
+tuples, with n as a parameter of this function.
 
 Call the function simulate_throws_from_sample_space.
 
 Question 3
-Your goal here is to implement a function simulate_throws, similar to the one you wrote in Question 2, but without generating a sample space at all - just using the face_values.
+Your goal here is to implement a function simulate_throws, similar to the one
+you wrote in Question 2, but without generating a sample space at all - just
+using the face_values.
 
 Write a function that implements this, and name it simulate_throws.
 
 Question 4
-Using both methods of generating throws, build a dictionary that contains the face values as keys, and the number of times they were selected in the simulated throws.
+Using both methods of generating throws, build a dictionary that contains the
+face values as keys, and the number of times they were selected in the
+simulated throws.
 
-For example, assuming you made 100 throws using one of these methods, your dictionary might look like this:
+For example, assuming you made 100 throws using one of these methods, your
+dictionary might look like this:
 
 {
     '9': 39,
@@ -46,10 +55,13 @@ For example, assuming you made 100 throws using one of these methods, your dicti
     'K': 36,
     'A': 36
 }
-Note that your values in the dictionary should add up to 200 is you made one 100 throws.
+Note that your values in the dictionary should add up to 200 is you made one
+100 throws.
 
-Write a function that is given the function to use to generate the throws, the number of throws to simulate, and returns this dictionary.
+Write a function that is given the function to use to generate the throws, the
+number of throws to simulate, and returns this dictionary.
 
 Question 5
-Write a function that given two arguments a and b returns a random float between a (inclusive) and b (exclusive).
+Write a function that given two arguments a and b returns a random float
+between a (inclusive) and b (exclusive).
 '''

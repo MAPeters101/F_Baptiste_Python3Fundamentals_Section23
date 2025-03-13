0a69cdd7ec344e0693d3ccac30ef528ec6310040
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:34:41 -0400

    Description for all exercise01.

---
+++ exercise01.py
@@ -0,0 +1,25 @@
+'''
+Question 1
+Generate the sample space of rolling two 6-sided dice, numbered '9', '10', 'J',
+'Q', 'K', 'A'.
+
+(The sample space is the set of all possible outcomes).
+
+Your result should be a list containing tuples for the outcome of each die,
+e.g.
+
+[('9', '9'),
+ ('9', '10'),
+ ('9', 'J'),
+ ('9', 'Q'),
+ ('9', 'K'),
+ ('9', 'A'),
+ ('10', '9'),
+ ('10', '10'),
+ ('10', 'J'),
+ ('10', 'Q'),
+ ('10', 'K'),
+ ('10', 'A'),
+ etc
+Make this a function that returns the sample space, called make_sample_space.
+'''

beb4eb2ecdbdb2b72d39e01d2b4897599b03fb32
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:36:40 -0400

    Description for all exercise03.

---
+++ exercise03.py
@@ -0,0 +1,8 @@
+'''
+Question 3
+Your goal here is to implement a function simulate_throws, similar to the one
+you wrote in Question 2, but without generating a sample space at all - just
+using the face_values.
+
+Write a function that implements this, and name it simulate_throws.
+'''

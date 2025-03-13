5c09e3d06329c2cf300663cdcad0e49a853d9e36
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:35:40 -0400

    Description for all exercise02.

---
+++ exercise02.py
@@ -0,0 +1,10 @@
+'''
+Question 2
+Using the sample space you just created above, simulate throwing the two die n
+times by making random choices from the sample space.
+
+Again, make this into a function that returns the random choices as a list of
+tuples, with n as a parameter of this function.
+
+Call the function simulate_throws_from_sample_space.
+'''
